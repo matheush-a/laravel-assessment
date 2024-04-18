30c635485afe418dc788764a60ccc62a3cd769a4
matheush-a <mathzalmeida@gmail.com>
2024-04-18 18:41:57 -0300

    feat: entities models

---
+++ app/Models/Book.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'isbn',
+        'value',
+    ];
+
+    public function stores(): BelongsToMany
+    {
+        return $this->belongsToMany(Store::class);
+    }
+}
